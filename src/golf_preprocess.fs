64d83d019401069710d4679ea0a51f7d07a38811
pew <pew@perl.internal.at>
2015-12-07 17:15:00 +0100

    added initial support for blocks

--- src/golf_preprocess.fs
+++ src/golf_preprocess.fs
@@ -6,6 +6,8 @@ S\" ^\"((?:\\\\.|[^\"])*)\"?" regex$ constant rgx-string-double
 S\" ^-?[0-9]+" regex$ constant rgx-integer
 S\" ^#[^\\n\\r]*" regex$ constant rgx-comment
 S\" ^:" regex$ constant rgx-store
+S\" ^\\{" regex$ constant rgx-block-start
+S\" ^\\}" regex$ constant rgx-block-end
 S\" ^." regex$ constant rgx-variable-char
 
 
@@ -15,7 +17,12 @@ s" +" , , s" golf_+" , ,
 0 ,
 
 
-create append-buffer 4096 chars allot
+
+: str-append { addr1 u1 addr2 u2 -- addr u }
+addr1 u1 u2 + dup { u } resize throw { addr }
+addr2 addr u1 + u2 move
+addr u ;
+
 
 : get-mapping { caddr u map-table --  caddr u | caddr1 u1 }
 
@@ -38,12 +45,19 @@ create append-buffer 4096 chars allot
 ;
 
 
-: append-with-spaces s" " append-buffer +place append-buffer +place ;
+
+\ standard signature ( buffer buffer-len addr u  -- )
+: append-with-spaces { buffer buffer-len addr u } buffer buffer-len s"  " str-append addr u str-append ;
+
 : execute-variable  mapping-operators get-mapping append-with-spaces ;
-: execute-string  S\"  S\\\" " append-buffer +place append-buffer +place S\" \"" append-buffer +place ;
-: execute-integer append-with-spaces s"  anon_int"  append-buffer +place ;
-: execute-store s"  create " append-buffer +place append-buffer +place s"  ," append-buffer +place ;
+
+: execute-string  { buf buf-len addr u } buf buf-len S\"  S\\\" " str-append addr u str-append S\" \" anon_str " str-append ;
+: execute-integer ( buf buf-len addr u -- adr1 u1)  str-append s"  anon_int"  str-append ;
+: execute-store   { buf buf-len addr u }  buf buf-len s"  create " str-append addr u str-append s"  ," str-append ;
 : execute-comment 2drop ;
+: execute-block-start 2drop ;
+: execute-block-end 2drop ;
+
 
 
 create token-rules
@@ -52,6 +66,9 @@ rgx-string-single   , 1 , ' execute-string ,   \ string - single quotes
 rgx-string-double   , 1 , ' execute-string ,   \ string - double quotes
 rgx-integer         , 0 , ' execute-integer ,  \ integer
 rgx-comment         , 0 , ' execute-comment ,  \ comment
+rgx-store           , 0 , ' execute-store ,    \ : store variables 
+rgx-block-start     , 0 , ' execute-block-start ,    \ block treatment
+rgx-block-end       , 0 , ' execute-block-end ,  
 rgx-store           , 0 , ' execute-store ,    \ : store variables 
 rgx-variable-char   , 0 , ' execute-variable , \ variable - char variant
 0 ,
@@ -98,44 +115,89 @@ rgx-variable-char   , 0 , ' execute-variable , \ variable - char variant
 
 
 
-: execute-token ( caddr u -- caddr1 u1 flag )
+: execute-token { caddr u buffer buffer-len -- caddr1 u1 append-addr1 append-u1 flag }
 
- get-execute-token
- 
- dup if
-    \ special case store operation ':' 
-    dup ['] execute-store = if
+    caddr u
 
-        drop 2drop              \ we throw away the execute and the string 
-        get-execute-token if    \ no match no problem, see reference implementation in ruby ;)
+    get-execute-token
 
-            execute-store
+    \ exit if nothing was found
+    dup 0= if drop buffer buffer-len 0 EXIT then
 
-        then
-        -1
+    \ standard execute stack:
+    \ ( buffer buffer-len addr u  -- )
+    CASE 
+    
+        \ 
+        \ special case: store operation ':' 
+        \ 
+        ['] execute-store OF
+            2drop              \ we throw away the execute and the string 
+            get-execute-token if    \ no match no problem, see reference implementation in ruby ;)
+
+                buffer execute-store
+            then
+
+            -1
+        ENDOF
+          
+        \ 
+        \ special case: block start '{' 
+        \ 
+        ['] execute-block-start OF
+            2drop
+
+            4096 chars allocate 
+
+            begin
+                recurse dup
+                -2 = if drop 0 then
+            while
+            repeat
+
+            buffer 
+            buffer-len 
+
+            S\" S\\\" " str-append
+
+            2swap str-append
+
+            S\" \" anon_block" str-append
+
+            -1
+        ENDOF
+
+        
+        ['] execute-block-end OF
+            2drop
+            buffer buffer-len
+            -2
+        ENDOF
+
+    \ 
     \ standard execution
-    else
-        execute -1
-    then
+    \ 
+    { addr u xt }
+    buffer buffer-len addr u xt 
 
- else 
-    drop 0
- then 
+    execute -1
+    0 \ this is a dummy for drop in the endcase
+    ENDCASE 
+
 ;
-
-
 
 
 : golf-preprocess ( caddr u -- caddr1 u1 )
 
-s" " append-buffer place \ reset append buffer
+4096 chars allocate \ start buffer
 
 begin 
     execute-token
     while 
 repeat  
-    2drop
+    rot drop
+    rot drop
 
-append-buffer count
+
 ;
 
